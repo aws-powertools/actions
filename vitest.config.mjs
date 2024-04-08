63ac07288390417e7384e4b57192c5be4d11c87d
Andrea Amorosi <dreamorosi@gmail.com>
2024-04-08 18:18:41 +0200

    chore: update vitest config

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    include: ['.github/scripts/tests/*.test.{js,mjs}']
+    include: [".github/scripts/tests/*.test.{js,mjs}"],
   },
-})
+});
 
 // Big rocks
 // 1. Incubating solutions to customers / internal teams
 // 2. Accelerating productivity to sales teams
-// 3. 
+// 3.
 
 // [Battlecard]
 // when to use Powertools
