86c92fd3f0e23f3fd58786f3b3871432e857e170
heitorlessa <lessa@amazon.co.uk>
2024-04-09 17:12:26 +0200

    chore: setup npm workspaces
    Signed-off-by: heitorlessa <lessa@amazon.co.uk>

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -2,15 +2,6 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    include: [".github/scripts/tests/*.test.{js,mjs}"],
+    include: ["packages/*/tests/*.test.{js,mjs}"],
   },
 });
-
-// Big rocks
-// 1. Incubating solutions to customers / internal teams
-// 2. Accelerating productivity to sales teams
-// 3.
-
-// [Battlecard]
-// when to use Powertools
-// why using Powertools
