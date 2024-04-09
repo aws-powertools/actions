614e7763a1a2caf3ea353909fefc91c395486348
heitorlessa <lessa@amazon.co.uk>
2024-04-09 17:33:37 +0200

    chore(ci): add biome
    Signed-off-by: heitorlessa <lessa@amazon.co.uk>

--- packages/monthly_roadmap/src/index.mjs
+++ packages/monthly_roadmap/src/index.mjs
@@ -1,10 +1,10 @@
 const DEFAULT_EMPTY_RESPONSE = [{}];
 const MONTH = new Date().toLocaleString("default", { month: "long" });
 const BLOCKED_LABELS = [
-  "do-not-merge",
-  "need-issue",
-  "need-rfc",
-  "need-customer-feedback",
+	"do-not-merge",
+	"need-issue",
+	"need-rfc",
+	"need-customer-feedback",
 ];
 
 /**
@@ -14,10 +14,10 @@ const BLOCKED_LABELS = [
  * @returns {number} - The difference in days between the current date and the given datetime.
  */
 const diffInDays = (datetime) => {
-  const diff_in_ms = new Date() - new Date(datetime);
+	const diff_in_ms = new Date() - new Date(datetime);
 
-  // ms(1000)->seconds(60)->minutes(60)->hours(24)->days
-  return Math.floor(diff_in_ms / (1000 * 60 * 60 * 24));
+	// ms(1000)->seconds(60)->minutes(60)->hours(24)->days
+	return Math.floor(diff_in_ms / (1000 * 60 * 60 * 24));
 };
 
 /**
@@ -31,8 +31,8 @@ const diffInDays = (datetime) => {
  * console.log(formatDate(datetime)); // April 5, 2024
  */
 export const formatDate = (datetime) => {
-  const date = new Date(datetime);
-  return new Intl.DateTimeFormat("en-US", { dateStyle: "long" }).format(date);
+	const date = new Date(datetime);
+	return new Intl.DateTimeFormat("en-US", { dateStyle: "long" }).format(date);
 };
 
 /**
@@ -59,20 +59,20 @@ export const formatDate = (datetime) => {
  * console.log(table.rows); // '| John | 30  | New York |'
  */
 const buildMarkdownTable = (data) => {
-  const keys = Object.keys(data[0]);
+	const keys = Object.keys(data[0]);
 
-  if (keys.length === 0) {
-    return "Not available";
-  }
+	if (keys.length === 0) {
+		return "Not available";
+	}
 
-  const formatted_headings = `${keys.join(" | ")}`;
-  const keyLength = keys[0]?.length || 0;
-  const dashes = keys.map(() => `${"-".repeat(keyLength)} |`).join(" ");
+	const formatted_headings = `${keys.join(" | ")}`;
+	const keyLength = keys[0]?.length || 0;
+	const dashes = keys.map(() => `${"-".repeat(keyLength)} |`).join(" ");
 
-  const values = data.map((issues) => Object.values(issues));
-  const rows = values.map((row) => `${row.join(" | ")} |`).join("\n");
+	const values = data.map((issues) => Object.values(issues));
+	const rows = values.map((row) => `${row.join(" | ")} |`).join("\n");
 
-  return `${formatted_headings}
+	return `${formatted_headings}
 ${dashes}
 ${rows}`;
 };
@@ -90,26 +90,26 @@ ${rows}`;
  *
  */
 async function getTopFeatureRequests({ github, context, core }) {
-  core.info("Fetching feature requests sorted by +1 reactions");
+	core.info("Fetching feature requests sorted by +1 reactions");
 
-  const { data: issues } = await github.rest.issues.listForRepo({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    labels: "feature-request",
-    sort: "reactions-+1",
-    direction: "desc",
-    per_page: 3,
-  });
+	const { data: issues } = await github.rest.issues.listForRepo({
+		owner: context.repo.owner,
+		repo: context.repo.repo,
+		labels: "feature-request",
+		sort: "reactions-+1",
+		direction: "desc",
+		per_page: 3,
+	});
 
-  core.info("Successfully fetched issues");
-  core.debug(issues);
+	core.info("Successfully fetched issues");
+	core.debug(issues);
 
-  return issues.map((issue) => ({
-    title: `[${issue.title}](${issue.html_url})`,
-    created_at: formatDate(issue.created_at),
-    reaction_count: issue.reactions.total_count,
-    labels: `${issue.labels.map((label) => `\`${label.name}\``).join(",")}`, // enclose each label with `<label>` for rendering
-  }));
+	return issues.map((issue) => ({
+		title: `[${issue.title}](${issue.html_url})`,
+		created_at: formatDate(issue.created_at),
+		reaction_count: issue.reactions.total_count,
+		labels: `${issue.labels.map((label) => `\`${label.name}\``).join(",")}`, // enclose each label with `<label>` for rendering
+	}));
 }
 
 /**
@@ -125,25 +125,25 @@ async function getTopFeatureRequests({ github, context, core }) {
  *
  */
 async function getTopMostCommented({ github, context, core }) {
-  core.info("Fetching issues sorted by comments");
+	core.info("Fetching issues sorted by comments");
 
-  const { data: issues } = await github.rest.issues.listForRepo({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    sort: "comments",
-    direction: "desc",
-    per_page: 3,
-  });
+	const { data: issues } = await github.rest.issues.listForRepo({
+		owner: context.repo.owner,
+		repo: context.repo.repo,
+		sort: "comments",
+		direction: "desc",
+		per_page: 3,
+	});
 
-  core.info("Successfully fetched issues");
-  core.debug(issues);
+	core.info("Successfully fetched issues");
+	core.debug(issues);
 
-  return issues.map((issue) => ({
-    title: `[${issue.title}](${issue.html_url})`,
-    created_at: formatDate(issue.created_at),
-    comment_count: issue.comments,
-    labels: `${issue.labels.map((label) => `\`${label.name}\``).join(",")}`, // enclose each label with `<label>` for rendering
-  }));
+	return issues.map((issue) => ({
+		title: `[${issue.title}](${issue.html_url})`,
+		created_at: formatDate(issue.created_at),
+		comment_count: issue.comments,
+		labels: `${issue.labels.map((label) => `\`${label.name}\``).join(",")}`, // enclose each label with `<label>` for rendering
+	}));
 }
 
 /**
@@ -159,37 +159,37 @@ async function getTopMostCommented({ github, context, core }) {
  * @returns {Promise<Array<Response>>} A promise resolving with an array of issue objects.
  */
 async function getTopOldestIssues({ github, context, core }) {
-  core.info("Fetching issues sorted by creation date");
-  const { data: issues } = await github.rest.issues.listForRepo({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    sort: "created",
-    direction: "asc",
-    per_page: 30,
-  });
+	core.info("Fetching issues sorted by creation date");
+	const { data: issues } = await github.rest.issues.listForRepo({
+		owner: context.repo.owner,
+		repo: context.repo.repo,
+		sort: "created",
+		direction: "asc",
+		per_page: 30,
+	});
 
-  core.info("Successfully fetched issues");
-  core.debug(issues);
+	core.info("Successfully fetched issues");
+	core.debug(issues);
 
-  core.info(
-    `Filtering out issues that contained blocking labels: ${BLOCKED_LABELS}`
-  );
-  const top3 = issues
-    .filter((issue) =>
-      issue.labels.every((label) => !BLOCKED_LABELS.includes(label.name))
-    )
-    .slice(0, 3);
+	core.info(
+		`Filtering out issues that contained blocking labels: ${BLOCKED_LABELS}`,
+	);
+	const top3 = issues
+		.filter((issue) =>
+			issue.labels.every((label) => !BLOCKED_LABELS.includes(label.name)),
+		)
+		.slice(0, 3);
 
-  core.debug(top3);
+	core.debug(top3);
 
-  return top3.map((issue) => {
-    return {
-      title: `[${issue.title}](${issue.html_url})`,
-      created_at: formatDate(issue.created_at),
-      last_update: `${diffInDays(issue.updated_at)} days`,
-      labels: `${issue.labels.map((label) => `\`${label.name}\``).join(",")}`, // enclose each label with `<label>` for rendering
-    };
-  });
+	return top3.map((issue) => {
+		return {
+			title: `[${issue.title}](${issue.html_url})`,
+			created_at: formatDate(issue.created_at),
+			last_update: `${diffInDays(issue.updated_at)} days`,
+			labels: `${issue.labels.map((label) => `\`${label.name}\``).join(",")}`, // enclose each label with `<label>` for rendering
+		};
+	});
 }
 
 /**
@@ -205,36 +205,36 @@ async function getTopOldestIssues({ github, context, core }) {
  * @returns {Promise<Array<Response>>} A promise resolving with an array of PR objects.
  */
 async function getLongRunningPRs({ github, context, core }) {
-  core.info("Fetching PRs sorted by long-running");
-  const { data: prs } = await github.rest.pulls.list({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    sort: "long-running",
-    direction: "desc",
-    per_page: 30,
-  });
+	core.info("Fetching PRs sorted by long-running");
+	const { data: prs } = await github.rest.pulls.list({
+		owner: context.repo.owner,
+		repo: context.repo.repo,
+		sort: "long-running",
+		direction: "desc",
+		per_page: 30,
+	});
 
-  core.debug(issues);
+	core.debug(issues);
 
-  core.info(
-    `Filtering out issues that contained blocking labels: ${BLOCKED_LABELS}`
-  );
-  const top3 = prs
-    .filter((pr) =>
-      pr.labels.every((label) => !BLOCKED_LABELS.includes(label.name))
-    )
-    .slice(0, 3);
+	core.info(
+		`Filtering out issues that contained blocking labels: ${BLOCKED_LABELS}`,
+	);
+	const top3 = prs
+		.filter((pr) =>
+			pr.labels.every((label) => !BLOCKED_LABELS.includes(label.name)),
+		)
+		.slice(0, 3);
 
-  core.debug(top3);
+	core.debug(top3);
 
-  return top3.map((issue) => {
-    return {
-      title: `[${issue.title}](${issue.html_url})`,
-      created_at: formatDate(issue.created_at),
-      last_update: `${diffInDays(issue.updated_at)} days`,
-      labels: `${issue.labels.map((label) => `\`${label.name}\``).join(",")}`, // enclose each label with `<label>` for rendering
-    };
-  });
+	return top3.map((issue) => {
+		return {
+			title: `[${issue.title}](${issue.html_url})`,
+			created_at: formatDate(issue.created_at),
+			last_update: `${diffInDays(issue.updated_at)} days`,
+			labels: `${issue.labels.map((label) => `\`${label.name}\``).join(",")}`, // enclose each label with `<label>` for rendering
+		};
+	});
 }
 
 /**
@@ -247,28 +247,28 @@ async function getLongRunningPRs({ github, context, core }) {
  *
  */
 async function createMonthlyRoadmapReport({ github, context, core }) {
-  core.info("Fetching GitHub data concurrently");
+	core.info("Fetching GitHub data concurrently");
 
-  const [
-    { value: featureRequests = DEFAULT_EMPTY_RESPONSE },
-    { value: longRunningPRs = DEFAULT_EMPTY_RESPONSE },
-    { value: oldestIssues = DEFAULT_EMPTY_RESPONSE },
-    { value: mostActiveIssues = DEFAULT_EMPTY_RESPONSE },
-  ] = await Promise.allSettled([
-    getTopFeatureRequests({ github, context, core }),
-    getLongRunningPRs({ github, context, core }),
-    getTopOldestIssues({ github, context, core }),
-    getTopMostCommented({ github, context, core }),
-  ]);
+	const [
+		{ value: featureRequests = DEFAULT_EMPTY_RESPONSE },
+		{ value: longRunningPRs = DEFAULT_EMPTY_RESPONSE },
+		{ value: oldestIssues = DEFAULT_EMPTY_RESPONSE },
+		{ value: mostActiveIssues = DEFAULT_EMPTY_RESPONSE },
+	] = await Promise.allSettled([
+		getTopFeatureRequests({ github, context, core }),
+		getLongRunningPRs({ github, context, core }),
+		getTopOldestIssues({ github, context, core }),
+		getTopMostCommented({ github, context, core }),
+	]);
 
-  const tables = {
-    featureRequests: buildMarkdownTable(featureRequests),
-    mostActiveIssues: buildMarkdownTable(mostActiveIssues),
-    longRunningPRs: buildMarkdownTable(longRunningPRs),
-    oldestIssues: buildMarkdownTable(oldestIssues),
-  };
+	const tables = {
+		featureRequests: buildMarkdownTable(featureRequests),
+		mostActiveIssues: buildMarkdownTable(mostActiveIssues),
+		longRunningPRs: buildMarkdownTable(longRunningPRs),
+		oldestIssues: buildMarkdownTable(oldestIssues),
+	};
 
-  const body = `
+	const body = `
 
 Quick report of top 3 issues/PRs to assist in roadmap updates. Issues or PRs with the following labels are excluded:
 
@@ -296,26 +296,26 @@ ${tables.longRunningPRs}
 ${tables.oldestIssues}
   `;
 
-  core.info("Creating issue with monthly roadmap report");
+	core.info("Creating issue with monthly roadmap report");
 
-  // let ret = await github.rest.issues.create({
-  //   owner: context.repo.owner,
-  //   repo: context.repo.repo,
-  //   title: `Roadmap update reminder - ${MONTH}`,
-  //   body,
-  // });
+	// let ret = await github.rest.issues.create({
+	//   owner: context.repo.owner,
+	//   repo: context.repo.repo,
+	//   title: `Roadmap update reminder - ${MONTH}`,
+	//   body,
+	// });
 
-  await core.summary
-    .addHeading("Monthly roadmap reminder created")
-    .addLink("View monthly report", ret.data.html_url)
-    .write();
+	await core.summary
+		.addHeading("Monthly roadmap reminder created")
+		.addLink("View monthly report", ret.data.html_url)
+		.write();
 
-  return ret;
+	return ret;
 }
 
 /** @param {import('@types/github-script').AsyncFunctionArguments} AsyncFunctionArguments */
 export default async function main({ github, context, core }) {
-  return await createMonthlyRoadmapReport({ github, context, core });
+	return await createMonthlyRoadmapReport({ github, context, core });
 }
 
 // // @ts-check
--- packages/monthly_roadmap/tests/monthly_roadmap.test.mjs
+++ packages/monthly_roadmap/tests/monthly_roadmap.test.mjs
@@ -1,7 +1,7 @@
-import { formatDate } from "../src/index.mjs";
 import { expect, test } from "vitest";
+import { formatDate } from "../src/index.mjs";
 
 test("format date", () => {
-  let date = new Date("2024-01-01T12:00:00Z");
-  expect(formatDate(date)).toBe("January 1, 2025");
+	const date = new Date("2024-01-01T12:00:00Z");
+	expect(formatDate(date)).toBe("January 1, 2025");
 });
