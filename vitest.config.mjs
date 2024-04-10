907e62dc46e7c83a75172db9bea614f2a2c0ec1e
heitorlessa <lessa@amazon.co.uk>
2024-04-10 11:41:58 +0200

    chore: increase timeout to 10s
    Signed-off-by: heitorlessa <lessa@amazon.co.uk>

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  test: {
-    include: ["packages/*/tests/*.test.{js,mjs}"],
-  },
+	test: {
+		include: ["packages/*/tests/*.test.{js,mjs}"],
+		testTimeout: 10000,
+	},
 });
