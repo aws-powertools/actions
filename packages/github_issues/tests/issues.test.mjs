9c40bcf88ef5236cd5d1f565ae9e61b4670db5f8
heitorlessa <lessa@amazon.co.uk>
2024-04-18 14:43:21 +0200

    chore(test): rename to contract testing (list issues)
    Signed-off-by: heitorlessa <lessa@amazon.co.uk>

--- packages/github_issues/tests/issues.test.mjs
+++ packages/github_issues/tests/issues.test.mjs
@@ -6,129 +6,13 @@ import { buildIssues, buildSearchIssues } from "../../testing/src/builders/issue
 import {
 	findIssueFailureHandler,
 	findIssueHandler,
-	listIssuesFailureHandler,
-	listIssuesHandler,
 	updateIssueFailureHandler,
 	updateIssueHandler,
 } from "../../testing/src/interceptors/issues_handler.mjs";
-import { MAX_ISSUES_PER_PAGE } from "../src/constants.mjs";
-import { findIssue, listIssues, updateIssue } from "../src/issues.mjs";
+import { findIssue, updateIssue } from "../src/issues.mjs";
 
 const org = "aws-powertools";
 const repo = "powertools-lambda-python";
-
-describe("list issues", () => {
-	const server = setupServer();
-
-	beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
-
-	afterAll(() => server.close());
-
-	afterEach(() => server.resetHandlers());
-
-	it("should list issues (default parameters)", async () => {
-		// GIVEN
-		const data = buildIssues({ max: 5 });
-		server.use(...listIssuesHandler({ data, org, repo }));
-
-		// WHEN
-		const ret = await listIssues({
-			github: buildGithubClient({ token: process.env.GITHUB_TOKEN }),
-			context: buildGithubContext({ org, repo }),
-			core: buildGithubCore(),
-		});
-
-		// THEN
-		expect(ret).toStrictEqual(data);
-	});
-
-	it("should filter out pull requests from results", async () => {
-		// GIVEN
-		const realIssues = buildIssues({ max: 2 });
-		const prAsIssues = buildIssues({ max: 2, isPr: true });
-		server.use(...listIssuesHandler({ data: [...realIssues, ...prAsIssues], org, repo }));
-
-		// WHEN
-		const ret = await listIssues({
-			github: buildGithubClient({ token: process.env.GITHUB_TOKEN }),
-			context: buildGithubContext({ org, repo }),
-			core: buildGithubCore(),
-			labels: ["feature"],
-		});
-
-		// THEN
-		expect(ret).toStrictEqual(realIssues);
-	});
-
-	it("should exclude results with certain labels", async () => {
-		const BLOCKED_LABELS = "do-not-merge";
-
-		const data = buildIssues({ max: 5, labels: [BLOCKED_LABELS] });
-		server.use(...listIssuesHandler({ data, org, repo }));
-
-		// WHEN
-		const ret = await listIssues({
-			github: buildGithubClient({ token: process.env.GITHUB_TOKEN }),
-			context: buildGithubContext({ org, repo }),
-			core: buildGithubCore(),
-			excludeLabels: BLOCKED_LABELS,
-		});
-
-		// THEN
-		expect(ret.length).toBe(0);
-	});
-
-	it("should limit the number of issues returned", async () => {
-		const data = buildIssues({ max: 2 });
-		server.use(...listIssuesHandler({ data, org, repo }));
-
-		// WHEN
-		const ret = await listIssues({
-			github: buildGithubClient({ token: process.env.GITHUB_TOKEN }),
-			context: buildGithubContext({ org, repo }),
-			core: buildGithubCore(),
-			limit: 1,
-		});
-
-		// THEN
-		expect(ret.length).toBe(1);
-	});
-
-	it("should paginate to list all available issues when the limit is higher", async () => {
-		// GIVEN
-		const totalIssues = MAX_ISSUES_PER_PAGE + 1;
-
-		const data = buildIssues({ max: totalIssues });
-		server.use(...listIssuesHandler({ data, org, repo }));
-
-		// WHEN
-		const ret = await listIssues({
-			github: buildGithubClient({ token: process.env.GITHUB_TOKEN }),
-			context: buildGithubContext({ org, repo }),
-			core: buildGithubCore(),
-			limit: totalIssues,
-		});
-
-		// THEN
-		expect(ret.length).toBe(totalIssues);
-	});
-
-	it("should throw error when GitHub API call fails (http 500)", async () => {
-		// GIVEN
-		const err = "Unable to process request at this time";
-		server.use(...listIssuesFailureHandler({ org, repo, err }));
-
-		// WHEN
-		// THEN
-		const ret = await expect(
-			listIssues({
-				github: buildGithubClient({ token: process.env.GITHUB_TOKEN }),
-				context: buildGithubContext({ org, repo }),
-				core: buildGithubCore(),
-			}),
-		).rejects.toThrowError(err);
-	});
-});
 
 describe("search issues", () => {
 	const server = setupServer();
