0f35dbce934325e5abc700e4889bb6d9eb2b09c6
heitorlessa <lessa@amazon.co.uk>
2024-04-18 15:10:02 +0200

    chore(test): cleanup update_issue test
    Signed-off-by: heitorlessa <lessa@amazon.co.uk>

--- packages/github_issues/tests/contract/update_issue.test.mjs
+++ packages/github_issues/tests/contract/update_issue.test.mjs
@@ -1,15 +1,18 @@
 import { setupServer } from "msw/node";
 import { afterAll, afterEach, beforeAll, describe, expect, it } from "vitest";
-import { buildGithubClient, buildGithubContext, buildGithubCore } from "../../testing/src/builders/github_core.mjs";
-import { buildIssues } from "../../testing/src/builders/issues.mjs";
-import { updateIssueFailureHandler, updateIssueHandler } from "../../testing/src/interceptors/issues_handler.mjs";
-import { updateIssue } from "../src/issues.mjs";
+import { buildGithubClient, buildGithubContext, buildGithubCore } from "../../../testing/src/builders/github_core.mjs";
+import { buildIssues } from "../../../testing/src/builders/issues.mjs";
+import { updateIssueFailureHandler, updateIssueHandler } from "../../../testing/src/interceptors/issues_handler.mjs";
+import { updateIssue } from "../../src/issues.mjs";
 
 const org = "aws-powertools";
 const repo = "powertools-lambda-python";
 
 describe("update issues contract", () => {
 	const server = setupServer();
+	const github = buildGithubClient({ token: process.env.GITHUB_TOKEN });
+	const context = buildGithubContext({ org, repo });
+	const core = buildGithubCore();
 
 	beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
 
@@ -19,20 +22,21 @@ describe("update issues contract", () => {
 
 	it("should update an issue (default parameters)", async () => {
 		// GIVEN
-		const issue = buildIssues({ max: 1 })[0];
-		server.use(...updateIssueHandler({ data: issue, issueNumber: issue.number, org, repo }));
+		const updatedIssue = buildIssues({ max: 1 })[0];
+		server.use(...updateIssueHandler({ data: updatedIssue, issueNumber: updatedIssue.number, org, repo }));
 
 		// WHEN
 		const ret = await updateIssue({
-			github: buildGithubClient({ token: process.env.GITHUB_TOKEN }),
-			context: buildGithubContext({ org, repo }),
-			core: buildGithubCore(),
-			issueNumber: issue.number,
+			github,
+			context,
+			core,
+			issueNumber: updatedIssue.number,
 		});
 
 		// THEN
-		expect(ret).toStrictEqual(issue);
+		expect(ret).toStrictEqual(updatedIssue);
 	});
+
 	it("should throw error when GitHub API call fails (http 500)", async () => {
 		// GIVEN
 		const issueNumber = 0;
@@ -43,9 +47,9 @@ describe("update issues contract", () => {
 		// THEN
 		await expect(
 			updateIssue({
-				github: buildGithubClient({ token: process.env.GITHUB_TOKEN }),
-				context: buildGithubContext({ org, repo }),
-				core: buildGithubCore(),
+				github,
+				context,
+				core,
 				issueNumber,
 			}),
 		).rejects.toThrowError(err);
@@ -57,9 +61,9 @@ describe("update issues contract", () => {
 		// THEN
 		await expect(
 			updateIssue({
-				github: buildGithubClient({ token: process.env.GITHUB_TOKEN }),
-				context: buildGithubContext({ org, repo }),
-				core: buildGithubCore(),
+				github,
+				context,
+				core,
 			}),
 		).rejects.toThrowError("Issue number is required");
 	});
