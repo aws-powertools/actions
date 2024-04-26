c534eadd927f12df48e11b15855abb21aaea8b5e
heitorlessa <lessa@amazon.co.uk>
2024-04-26 14:41:39 +0200

    chore(test): generator spy worksga packages

--- packages/github/tests/unit/list_issues_or_pull_requests.test.mjs
+++ packages/github/tests/unit/list_issues_or_pull_requests.test.mjs
@@ -1,51 +1,50 @@
 import { GitHub } from "github/src/client";
 import { buildIssues, buildPullRequests } from "testing/src/builders";
 import { describe, expect, it, vi } from "vitest";
-import {MAX_ISSUES_PER_PAGE} from "../../src/constants.mjs";
+import { MAX_ISSUES_PER_PAGE } from "../../src/constants.mjs";
 
 describe("list issue", () => {
 	it("list issues (default params)", async () => {
 		// GIVEN
-		const existingIssues = buildIssues({max: 2});
+		const existingIssues = buildIssues({ max: 2 });
 		const github = new GitHub();
 		const listIssuesOptions = {
 			owner: github.owner,
 			repo: github.repo,
 			per_page: MAX_ISSUES_PER_PAGE,
 			direction: "asc",
-		}
+			labels: undefined,
+			sort: undefined,
+		};
 
-		vi.spyOn(github.client.paginate, "iterator").mockImplementation(async function* ()  {
-			yield {  data: existingIssues }
+		const paginateSpy = vi.spyOn(github.client.paginate, "iterator").mockImplementation(async function* () {
+			yield { data: existingIssues };
 		});
 
-		const listIssuesSpy = vi.spyOn(github.client.rest.issues, "listForRepo")
-
 		// WHEN
-		const issues = await github.listIssues({github})
+		const issues = await github.listIssues({ github });
 
 		// THEN
-		expect(issues).toStrictEqual(existingIssues)
-
-		expect(listIssuesSpy).toHaveBeenCalledWith(
-			expect.objectContaining(listIssuesOptions)
-		)
+		expect(issues).toStrictEqual(existingIssues);
+		expect(paginateSpy).toHaveBeenCalledWith(
+			github.client.rest.issues.listForRepo,
+			expect.objectContaining(listIssuesOptions),
+		);
 	});
 
 	it.todo("list pull requests", async () => {
 		// GIVEN
-		const existingPullRequests = buildPullRequests({max: 2});
+		const existingPullRequests = buildPullRequests({ max: 2 });
 		const github = new GitHub();
 		const listPullRequestsSpy = vi.spyOn(github, "listPullRequests").mockImplementation(() => existingPullRequests);
 
 		// WHEN
-		const pullRequests = github.listPullRequests({github})
+		const pullRequests = github.listPullRequests({ github });
 
 		// THEN
-		expect(pullRequests).toBe(existingPullRequests)
+		expect(pullRequests).toBe(existingPullRequests);
 		expect(listPullRequestsSpy).toHaveBeenCalledWith({
-			github
-		})
+			github,
+		});
 	});
-
 });
